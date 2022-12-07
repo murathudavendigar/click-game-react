09d28c731697f2a07a1913a0b1b9293e8688cf41
Captain Price <murathoncu@gmail.com>
2022-12-07 03:22:19 +0300

    add username page

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Result from "../pages/Result";
 import TopScores from "../pages/TopScores";
+import Username from "../pages/Username";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" index element={<Home />} />
+        <Route path="/" index element={<Username />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/result" element={<Result />} />
         <Route path="/topscores" element={<TopScores />} />
       </Routes>
