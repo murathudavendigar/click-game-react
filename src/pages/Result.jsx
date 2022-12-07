91004ae331872315bb707cd981318ac7ee9d4251
Captain Price <murathoncu@gmail.com>
2022-12-07 03:20:19 +0300

    add set and get Database method

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { useLocation, useNavigate } from "react-router-dom";
 import useDataCall from "../hooks/useDataCall";
 
 const Result = () => {
+  const { currentUser } = useSelector((state) => state.userName);
   const navigate = useNavigate();
   const { addData } = useDataCall();
   const location = useLocation();
   const { clickCount, userSelectTime } = location?.state;
   const point = clickCount / userSelectTime;
+  const info = {
+    userName: currentUser?.payload || "guest",
+    userSetTime: userSelectTime,
+    userClickCount: clickCount,
+    userPoint: point,
+  };
 
-  addData(point);
+  useEffect(() => {
+    addData(info);
+  }, []);
+
   return (
     <div
       style={{
@@ -18,7 +29,7 @@ const Result = () => {
         alignItems: "center",
       }}>
       <h1>Your Score is :{point}</h1>
-      <button onClick={() => navigate("/")}>Try again</button>
+      <button onClick={() => navigate("/home")}>Try again</button>
     </div>
   );
 };
